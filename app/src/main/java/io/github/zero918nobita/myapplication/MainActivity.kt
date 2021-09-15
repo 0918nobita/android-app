213d5482ffe140438b9233e1a11ff0030879b126
0918nobita <nobita.0918@gmail.com>
2021-09-15 13:29:36 +0900

    :sparkles: ボタンをクリックされたら TextView を更新するようにした

--- app/src/main/java/io/github/zero918nobita/myapplication/MainActivity.kt
+++ app/src/main/java/io/github/zero918nobita/myapplication/MainActivity.kt
@@ -2,10 +2,15 @@ package io.github.zero918nobita.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.text)
+        val btn = findViewById<Button>(R.id.button)
+        btn.setOnClickListener { textView.text = getString(R.string.clicked_msg) }
     }
 }
