29c83f5fdef3ede0aba085eb6e0c2181082fdad1
0918nobita <nobita.0918@gmail.com>
2021-09-16 15:08:50 +0900

    :recycle: Channel の代わりに MutableSharedFlow を使うようにした

--- app/src/main/java/io/github/zero918nobita/myapplication/MainViewModel.kt
+++ app/src/main/java/io/github/zero918nobita/myapplication/MainViewModel.kt
@@ -2,30 +2,27 @@ package io.github.zero918nobita.myapplication
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.stateIn
 
 class MainViewModel : ViewModel() {
     private val repository = MainRepository()
 
-    private val event = Channel<Unit>(Channel.BUFFERED)
+    private val event = MutableSharedFlow<Unit>()
 
-    private val textStateFlow = MutableStateFlow("Hello, world!")
-    init {
-        event.receiveAsFlow().onEach {
-            textStateFlow.value = repository.getStr()
-        }.launchIn(viewModelScope)
-    }
-    val text: Flow<String> get() = textStateFlow
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val text: Flow<String> =
+        event
+            .flatMapLatest { flow { emit(repository.getStr()) } }
+            .stateIn(viewModelScope, SharingStarted.Lazily, "Hello, world!")
 
     fun updateText() {
-        viewModelScope.launch {
-            event.send(Unit)
-        }
+        viewModelScope.launch { event.emit(Unit) }
     }
 }
